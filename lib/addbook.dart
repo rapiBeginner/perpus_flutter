c81d77b1a8b9dd69a86c2e53a10b92cd8dc9f888
rapiBeginner <araffi968@gmail.com>
2024-11-21 22:02:31 +0700

    create

---
+++ lib/addbook.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:perpustakaan/home_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class Addbook extends StatefulWidget {
+  const Addbook({super.key});
+  @override
+  _AddbookState createState() => _AddbookState();
+}
+
+class _AddbookState extends State<Addbook> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController judulController = TextEditingController();
+  final TextEditingController penulisController = TextEditingController();
+  final TextEditingController deskripsiController = TextEditingController();
+
+  Future<void> createBook() async {
+    if (formKey.currentState!.validate()) {
+      final response = await Supabase.instance.client.from('Buku').insert({
+        'judul': judulController.text,
+        'penulis': penulisController.text,
+        'deskripsi': deskripsiController.text,
+      });
+
+      if (response.error == null) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Buku telah ditambahkan'),
+        ));
+        await Future.delayed(const Duration(seconds: 1));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const BookListPage()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Gagal menambahkan buku: ${response.error!.message}'),
+        ));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tambah Buku')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: judulController,
+                decoration: const InputDecoration(labelText: 'Judul'),
+              ),
+              TextFormField(
+                controller: penulisController,
+                decoration: const InputDecoration(labelText: 'Penulis'),
+              ),
+              TextFormField(
+                controller: deskripsiController,
+                decoration: const InputDecoration(labelText: 'Deskripsi'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(onPressed: createBook, child: Text('Simpan'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:perpustakaan/addbook.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class BookListPage extends StatefulWidget {
@@ -9,6 +11,7 @@ class BookListPage extends StatefulWidget {
 
 class _BookListPageState extends State<BookListPage> {
   List<Map<String, dynamic>> buku = [];
+  int? hoveredIndex; // Untuk melacak indeks yang sedang di-hover
 
   @override
   void initState() {
@@ -27,59 +30,95 @@ class _BookListPageState extends State<BookListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Daftar buku'),
-          actions: [
-            IconButton(onPressed: fetchBooks, icon: const Icon(Icons.refresh))
-          ],
+      drawer: const Drawer(),
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        foregroundColor: Colors.white,
+        elevation: 10,
+        centerTitle: true,
+        title: Text(
+          'Daftar buku',
+          style: GoogleFonts.lora(fontWeight: FontWeight.bold),
         ),
-        body: buku.isEmpty
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: buku.length,
-                itemBuilder: (context, index) {
-                  final book = buku[index];
-                  return ListTile(
-                    title: Text(
-                      book['judul'] ?? 'No title',
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold),
+        actions: [
+          IconButton(onPressed: fetchBooks, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: buku.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: buku.length,
+              itemBuilder: (context, index) {
+                final book = buku[index];
+
+                return Card(
+                  elevation: 5,
+                  child: InkWell(
+                    onHover: (hovering) {
+                      setState(() {
+                        hoveredIndex = hovering ? index : null;
+                      });
+                    },
+                    onTap: () {
+                      // Aksi jika ListTile diklik
+                      // print('Tile ${book['judul']} diklik!');
+                    },
+                    child: ListTile(
+                      tileColor: hoveredIndex == index
+                          ? Colors.grey[300]
+                          : Colors.white, // Warna saat di-hover
+                      isThreeLine: true,
+                      title: Text(
+                        book['judul'] ?? 'No title',
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            book['penulis'] ?? 'No Author',
+                            style: const TextStyle(
+                                fontSize: 14, fontStyle: FontStyle.italic),
+                          ),
+                          Text(
+                            book['deskripsi'] ?? 'No description',
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(
+                                Icons.edit,
+                                color: Colors.blue,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ))
+                        ],
+                      ),
                     ),
-                    subtitle: Column(
-                      children: [
-                        Text(
-                          book['penulis'] ?? 'No Author',
-                          style: const TextStyle(
-                              fontSize: 14, fontStyle: FontStyle.italic),
-                        ),
-                        Text(
-                          book['deskripsi'] ?? 'No description',
-                          style: const TextStyle(fontSize: 12),
-                        ),
-                      ],
-                    ),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon:const Icon(
-                              Icons.edit,
-                              color: Colors.blue,
-                            )),
-                        IconButton(
-                            onPressed: () {},
-                            icon:const Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                            ))
-                      ],
-                    ),
-                  );
-                }));
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const Addbook())).then((_) {
+          fetchBooks();
+        });
+      },child: const Icon(Icons.add),),
+    );
   }
 }
